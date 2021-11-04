5425ce045f74ba5cb01a0380adfb56e3748eb19d
sarosh4271 <74011881+sarosh4271@users.noreply.github.com>
2021-11-04 21:51:46 +0500

    destini challenge app completed

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -10,9 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1ButtonUI: UIButton!
+    @IBOutlet weak var choice2ButtonUI: UIButton!
+    
+    var storiBrain = StoryBrain()
+   
+    
+    @IBAction func choiceButton(_ sender: UIButton) {
+//        if (choice1ButtonUI != nil){
+//            storiBrain.getNextStory(userChoice: choice1ButtonUI.currentTitle!)
+//        } else if (choice2ButtonUI != nil){
+//             storiBrain.getNextStory(userChoice: choice2ButtonUI.currentTitle!)
+//            updateUI()
+//        }
+//
+        storiBrain.getNextStory(userChoice: sender.currentTitle!)
+        updateUI()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
 
+    }
+    
+    func updateUI(){
+
+        storyLabel.text = storiBrain.getStoryTitle()
+        choice1ButtonUI.setTitle(storiBrain.getChoice1(), for: .normal)
+        choice2ButtonUI.setTitle(storiBrain.getChoice2(), for: .normal)
     }
 
 
